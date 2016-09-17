40369359c7be13097422c31154cbafc7a754df69
aaron <284565296@qq.com>
2016-09-17 22:28:21 +0800

    新增html5中只有在谷歌浏览器中起作用的上传文件夹案例

---
+++ 上传文件夹并在服务器里面保存上传的目录结构.php
@@ -0,0 +1,42 @@
+<?php
+if(isset($_FILES['upfile'])){
+	$_POST = explode("," ,$_POST['path'] );
+	
+	function upfile($tempfile , $path) {
+		
+		$path = explode("/" ,  $path );
+		if (!file_exists("upload")) {
+			@mkdir("upload");
+		}
+		if (!file_exists("upload/".$path[0])) {
+			@mkdir("upload/".$path[0] , 0777);
+		}
+		$path = implode("/" , $path );
+		$upfile = 'upload/' . $path;
+		
+		move_uploaded_file($tempfile , $upfile);
+		
+	}
+	array_map("upfile" , $_FILES['upfile']['tmp_name'] , $_POST  );
+}
+?>
+<form action="" enctype="multipart/form-data" method="post">
+<input type="file" name="upfile[]"  id="files" multiple webkitdirectory=""  />
+<input type="hidden" name="path" id="path" />
+<br/>
+<input type="submit" value="提交" />
+</form>
+
+<script src="//cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
+    <script>
+        $(function () {
+			var arr = Array();
+            $("#files").on("change",function (e) {
+                $.each(this.files,function (k,v) {
+					/*获得文件的相对路径*/
+                    arr[k] = v.webkitRelativePath;
+                });
+				$("#path").val(arr);
+            });
+        })
+    </script>
