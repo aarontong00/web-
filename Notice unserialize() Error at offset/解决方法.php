9ac07cd9f842a524b61e913c188b96358d1c5a90
aaron <284565296@qq.com>
2016-10-12 16:38:57 +0800

    新增反序列化报错解决方法

---
+++ Notice unserialize() Error at offset/解决方法.php
@@ -0,0 +1,102 @@
+<?php
+	$a = 'a:9:{i:1;a:8:{s:12:"module_title";a:1:{i:1;s:0:"";}s:11:"description";a:1:{i:1;s:3807:"&lt;p&gt;&lt;img alt=&quot;&quot; src=&quot;/image/data/0logo123554.png&quot; style=&quot;height:121px; margin-top:35px; width:158px&quot; /&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;select&gt;&lt;option value=&quot;&quot;&gt;Select Language&lt;/option&gt;&lt;option value=&quot;en|af&quot;&gt;Afrikaans&lt;/option&gt;&lt;option value=&quot;en|sq&quot;&gt;Albanian&lt;/option&gt;&lt;option value=&quot;en|ar&quot;&gt;Arabic&lt;/option&gt;&lt;option value=&quot;en|hy&quot;&gt;Armenian&lt;/option&gt;&lt;option value=&quot;en|az&quot;&gt;Azerbaijani&lt;/option&gt;&lt;option value=&quot;en|eu&quot;&gt;Basque&lt;/option&gt;&lt;option value=&quot;en|be&quot;&gt;Belarusian&lt;/option&gt;&lt;option value=&quot;en|bg&quot;&gt;Bulgarian&lt;/option&gt;&lt;option value=&quot;en|ca&quot;&gt;Catalan&lt;/option&gt;&lt;option value=&quot;en|zh-CN&quot;&gt;Chinese (Simplified)&lt;/option&gt;&lt;option value=&quot;en|zh-TW&quot;&gt;Chinese (Traditional)&lt;/option&gt;&lt;option value=&quot;en|hr&quot;&gt;Croatian&lt;/option&gt;&lt;option value=&quot;en|cs&quot;&gt;Czech&lt;/option&gt;&lt;option value=&quot;en|da&quot;&gt;Danish&lt;/option&gt;&lt;option value=&quot;en|nl&quot;&gt;Dutch&lt;/option&gt;&lt;option value=&quot;en|en&quot;&gt;English&lt;/option&gt;&lt;option value=&quot;en|et&quot;&gt;Estonian&lt;/option&gt;&lt;option value=&quot;en|tl&quot;&gt;Filipino&lt;/option&gt;&lt;option value=&quot;en|fi&quot;&gt;Finnish&lt;/option&gt;&lt;option value=&quot;en|fr&quot;&gt;French&lt;/option&gt;&lt;option value=&quot;en|gl&quot;&gt;Galician&lt;/option&gt;&lt;option value=&quot;en|ka&quot;&gt;Georgian&lt;/option&gt;&lt;option value=&quot;en|de&quot;&gt;German&lt;/option&gt;&lt;option value=&quot;en|el&quot;&gt;Greek&lt;/option&gt;&lt;option value=&quot;en|ht&quot;&gt;Haitian Creole&lt;/option&gt;&lt;option value=&quot;en|iw&quot;&gt;Hebrew&lt;/option&gt;&lt;option value=&quot;en|hi&quot;&gt;Hindi&lt;/option&gt;&lt;option value=&quot;en|hu&quot;&gt;Hungarian&lt;/option&gt;&lt;option value=&quot;en|is&quot;&gt;Icelandic&lt;/option&gt;&lt;option value=&quot;en|id&quot;&gt;Indonesian&lt;/option&gt;&lt;option value=&quot;en|ga&quot;&gt;Irish&lt;/option&gt;&lt;option value=&quot;en|it&quot;&gt;Italian&lt;/option&gt;&lt;option value=&quot;en|ja&quot;&gt;Japanese&lt;/option&gt;&lt;option value=&quot;en|ko&quot;&gt;Korean&lt;/option&gt;&lt;option value=&quot;en|lv&quot;&gt;Latvian&lt;/option&gt;&lt;option value=&quot;en|lt&quot;&gt;Lithuanian&lt;/option&gt;&lt;option value=&quot;en|mk&quot;&gt;Macedonian&lt;/option&gt;&lt;option value=&quot;en|ms&quot;&gt;Malay&lt;/option&gt;&lt;option value=&quot;en|mt&quot;&gt;Maltese&lt;/option&gt;&lt;option value=&quot;en|no&quot;&gt;Norwegian&lt;/option&gt;&lt;option value=&quot;en|fa&quot;&gt;Persian&lt;/option&gt;&lt;option value=&quot;en|pl&quot;&gt;Polish&lt;/option&gt;&lt;option value=&quot;en|pt&quot;&gt;Portuguese&lt;/option&gt;&lt;option value=&quot;en|ro&quot;&gt;Romanian&lt;/option&gt;&lt;option value=&quot;en|ru&quot;&gt;Russian&lt;/option&gt;&lt;option value=&quot;en|sr&quot;&gt;Serbian&lt;/option&gt;&lt;option value=&quot;en|sk&quot;&gt;Slovak&lt;/option&gt;&lt;option value=&quot;en|sl&quot;&gt;Slovenian&lt;/option&gt;&lt;option value=&quot;en|es&quot;&gt;Spanish&lt;/option&gt;&lt;option value=&quot;en|sw&quot;&gt;Swahili&lt;/option&gt;&lt;option value=&quot;en|sv&quot;&gt;Swedish&lt;/option&gt;&lt;option value=&quot;en|th&quot;&gt;Thai&lt;/option&gt;&lt;option value=&quot;en|tr&quot;&gt;Turkish&lt;/option&gt;&lt;option value=&quot;en|uk&quot;&gt;Ukrainian&lt;/option&gt;&lt;option value=&quot;en|ur&quot;&gt;Urdu&lt;/option&gt;&lt;option value=&quot;en|vi&quot;&gt;Vietnamese&lt;/option&gt;&lt;option value=&quot;en|cy&quot;&gt;Welsh&lt;/option&gt;&lt;option value=&quot;en|yi&quot;&gt;Yiddish&lt;/option&gt;&lt;/select&gt;&lt;/p&gt;
+";}s:10:"show_title";s:1:"1";s:9:"layout_id";s:5:"99999";s:8:"position";s:13:"footer_bottom";s:6:"status";s:1:"0";s:12:"module_class";s:0:"";s:10:"sort_order";s:1:"1";}i:2;a:8:{s:12:"module_title";a:1:{i:1;s:10:"MY ACCOUNT";}s:11:"description";a:1:{i:1;s:871:"&lt;div class=&quot;box&quot;&gt;
+&lt;ul&gt;
+  &lt;li&gt;&lt;a href=&quot;index.php?route=account/account&quot;&gt;&lt;span style=&quot;color:#7c7c7c&quot;&gt;My Account&lt;/span&gt;&lt;/a&gt;&lt;/li&gt;
+  &lt;li&gt;&lt;a href=&quot;index.php?route=account/order&quot;&gt;&lt;span style=&quot;color:#7c7c7c&quot;&gt;Order History&lt;/span&gt;&lt;/a&gt;&lt;/li&gt;
+ &lt;li&gt;&lt;a href=&quot;index.php?route=product/special&quot;&gt;&lt;span style=&quot;color:#7c7c7c&quot;&gt;Specials&lt;/span&gt;&lt;/a&gt;&lt;/li&gt;
+  &lt;li&gt;&lt;a href=&quot;index.php?route=account/wishlist&quot;&gt;&lt;span style=&quot;color:#7c7c7c&quot;&gt;Wish List&lt;/span&gt;&lt;/a&gt;&lt;/li&gt;
+  &lt;li&gt;&lt;a href=&quot;index.php?route=account/newsletter&quot;&gt;&lt;span style=&quot;color:#7c7c7c&quot;&gt;Newsletter&lt;/span&gt;&lt;/a&gt;&lt;/li&gt;
+&lt;/ul&gt;
+&lt;/div&gt;
+";}s:10:"show_title";s:1:"1";s:9:"layout_id";s:5:"99999";s:8:"position";s:13:"footer_bottom";s:6:"status";s:1:"0";s:12:"module_class";s:0:"";s:10:"sort_order";s:1:"2";}i:3;a:8:{s:12:"module_title";a:1:{i:1;s:10:"CONTACT US";}s:11:"description";a:1:{i:1;s:752:"&lt;div style=&quot;line-height:28px;&quot;&gt;&lt;img alt=&quot;&quot; src=&quot;/image/data/logo/1205c.png&quot; /&gt;&amp;nbsp; 1F, Blog. 16, Xinhua Economic&lt;br /&gt;
+&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; Park No.59-19 Shixiang Road,&lt;br /&gt;
+&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; Xiacheng District, Hangzhou&lt;br /&gt;
+&lt;img alt=&quot;&quot; src=&quot;/image/data/logo/1214logo.png&quot; style=&quot;height:28px; width:28px&quot; /&gt;&amp;nbsp; 0086 571-85826833&lt;br /&gt;
+&lt;img alt=&quot;&quot; src=&quot;/image/data/logo/1205b.png&quot; style=&quot;height:28px; margin-top:6px; width:28px&quot; /&gt;&amp;nbsp; yong@100yd.com.cn&lt;/div&gt;
+";}s:10:"show_title";s:1:"1";s:9:"layout_id";s:5:"99999";s:8:"position";s:13:"footer_bottom";s:6:"status";s:1:"0";s:12:"module_class";s:0:"";s:10:"sort_order";s:1:"4";}i:4;a:8:{s:12:"module_title";a:1:{i:1;s:10:"Contact Us";}s:11:"description";a:1:{i:1;s:974:"&lt;div style=&quot;padding:16px;&quot;&gt;
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Address:&lt;/strong&gt;&amp;nbsp;1F, Blog. 16, Xinhua Economic Park, No.59-19 Shixiang Road, Xiacheng District, Hangzhou, Zhejiang, China&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Tel:&lt;/strong&gt;&amp;nbsp;0086 571-85826833/0086 571-85826822&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Mob:&lt;/strong&gt;&amp;nbsp;0086 13357134555&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Skype:&lt;/strong&gt;&amp;nbsp;yearday002&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;E-mail: &lt;/strong&gt;yong@100yd.com.cn&lt;/span&gt;&lt;/p&gt;
+&lt;/div&gt;
+";}s:10:"show_title";s:1:"1";s:9:"layout_id";s:1:"3";s:8:"position";s:11:"column_left";s:6:"status";s:1:"1";s:12:"module_class";s:0:"";s:10:"sort_order";s:1:"3";}i:5;a:8:{s:12:"module_title";a:1:{i:1;s:10:"Contact Us";}s:11:"description";a:1:{i:1;s:983:"&lt;div style=&quot;padding:16px;&quot;&gt;
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Address:&lt;/strong&gt;&amp;nbsp;1F, Blog. 16, Xinhua Economic Park, No.59-19 Shixiang Road, Xiacheng District, Hangzhou, Zhejiang, China&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Tel:&lt;/strong&gt;&amp;nbsp;0086 571-85826833/0086 571-85826822&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Mob:&lt;/strong&gt;&amp;nbsp;0086 13357134555&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Skype:&lt;/strong&gt;&amp;nbsp;yearday002&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;E-mail:&lt;/strong&gt;&amp;nbsp;yong@100yd.com.cn&lt;/span&gt;&lt;/p&gt;
+&lt;/div&gt;
+";}s:10:"show_title";s:1:"1";s:9:"layout_id";s:2:"11";s:8:"position";s:11:"column_left";s:6:"status";s:1:"1";s:12:"module_class";s:0:"";s:10:"sort_order";s:1:"3";}i:6;a:8:{s:12:"module_title";a:1:{i:1;s:10:"Contact Us";}s:11:"description";a:1:{i:1;s:983:"&lt;div style=&quot;padding:16px;&quot;&gt;
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Address:&lt;/strong&gt;&amp;nbsp;1F, Blog. 16, Xinhua Economic Park, No.59-19 Shixiang Road, Xiacheng District, Hangzhou, Zhejiang, China&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Tel:&lt;/strong&gt;&amp;nbsp;0086 571-85826833/0086 571-85826822&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Mob:&lt;/strong&gt;&amp;nbsp;0086 13357134555&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Skype:&lt;/strong&gt;&amp;nbsp;yearday002&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;E-mail:&lt;/strong&gt;&amp;nbsp;yong@100yd.com.cn&lt;/span&gt;&lt;/p&gt;
+&lt;/div&gt;
+";}s:10:"show_title";s:1:"1";s:9:"layout_id";s:2:"12";s:8:"position";s:11:"column_left";s:6:"status";s:1:"1";s:12:"module_class";s:0:"";s:10:"sort_order";s:1:"3";}i:7;a:8:{s:12:"module_title";a:1:{i:1;s:10:"Contact Us";}s:11:"description";a:1:{i:1;s:983:"&lt;div style=&quot;padding:16px;&quot;&gt;
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Address:&lt;/strong&gt;&amp;nbsp;1F, Blog. 16, Xinhua Economic Park, No.59-19 Shixiang Road, Xiacheng District, Hangzhou, Zhejiang, China&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Tel:&lt;/strong&gt;&amp;nbsp;0086 571-85826833/0086 571-85826822&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Mob:&lt;/strong&gt;&amp;nbsp;0086 13357134555&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Skype:&lt;/strong&gt;&amp;nbsp;yearday002&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;E-mail:&lt;/strong&gt;&amp;nbsp;yong@100yd.com.cn&lt;/span&gt;&lt;/p&gt;
+&lt;/div&gt;
+";}s:10:"show_title";s:1:"1";s:9:"layout_id";s:1:"2";s:8:"position";s:11:"column_left";s:6:"status";s:1:"1";s:12:"module_class";s:0:"";s:10:"sort_order";s:1:"3";}i:8;a:8:{s:12:"module_title";a:1:{i:1;s:10:"Contact Us";}s:11:"description";a:1:{i:1;s:983:"&lt;div style=&quot;padding:16px;&quot;&gt;
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Address:&lt;/strong&gt;&amp;nbsp;1F, Blog. 16, Xinhua Economic Park, No.59-19 Shixiang Road, Xiacheng District, Hangzhou, Zhejiang, China&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Tel:&lt;/strong&gt;&amp;nbsp;0086 571-85826833/0086 571-85826822&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Mob:&lt;/strong&gt;&amp;nbsp;0086 13357134555&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;Skype:&lt;/strong&gt;&amp;nbsp;yearday002&lt;/span&gt;&lt;/p&gt;
+
+&lt;p&gt;&lt;span style=&quot;font-family:verdana,geneva,sans-serif&quot;&gt;&lt;strong&gt;E-mail:&lt;/strong&gt;&amp;nbsp;yong@100yd.com.cn&lt;/span&gt;&lt;/p&gt;
+&lt;/div&gt;
+";}s:10:"show_title";s:1:"1";s:9:"layout_id";s:2:"14";s:8:"position";s:11:"column_left";s:6:"status";s:1:"1";s:12:"module_class";s:0:"";s:10:"sort_order";s:1:"3";}i:9;a:8:{s:12:"module_title";a:1:{i:1;s:0:"";}s:11:"description";a:1:{i:1;s:1771:"&lt;div style=&quot;margin-top:40px;margin-buttom:40px;&quot;&gt;
+&lt;div class=&quot;col-lg-4 col-md-4 col-sm-12&quot; style=&quot;text-align: center;line-height:24px;&quot;&gt;&lt;img alt=&quot;&quot; src=&quot;/image/data/maidian/leimu-1.png&quot; style=&quot;height:120px; width:120px&quot; /&gt;&lt;br /&gt;
+&lt;br /&gt;
+&lt;strong&gt;&lt;span style=&quot;font-size:18px&quot;&gt;Ten years of Experience&lt;/span&gt;&lt;/strong&gt;&lt;br /&gt;
+&lt;span style=&quot;font-size:14px&quot;&gt;We try our best to meet all customer&amp;#39;s requirements in the printing and packaging of the rich experience.&lt;/span&gt;&lt;/div&gt;
+
+&lt;div class=&quot;col-lg-4 col-md-4 col-sm-12&quot; style=&quot;text-align: center;line-height:24px;&quot;&gt;&lt;img alt=&quot;&quot; src=&quot;/image/data/maidian/leimu-2.png&quot; style=&quot;height:120px; width:120px&quot; /&gt;&lt;br /&gt;
+&lt;br /&gt;
+&lt;span style=&quot;font-size:18px&quot;&gt;&lt;strong&gt;Customized Service&lt;/strong&gt;&lt;/span&gt;&lt;br /&gt;
+&lt;span style=&quot;font-size:14px&quot;&gt;Can be customized for various types of cards, labels, catalogs, brochures, paper bags, packing boxes.&lt;/span&gt;&lt;/div&gt;
+
+&lt;div class=&quot;col-lg-4 col-md-4 col-sm-12&quot; style=&quot;text-align: center;line-height:24px;&quot;&gt;&lt;img alt=&quot;&quot; src=&quot;/image/data/maidian/leimu-3.png&quot; style=&quot;height:120px; width:120px&quot; /&gt;&lt;br /&gt;
+&lt;br /&gt;
+&lt;span style=&quot;font-size:18px&quot;&gt;&lt;strong&gt;Aided Design&lt;/strong&gt;&lt;/span&gt;&lt;br /&gt;
+&lt;span style=&quot;font-size:14px&quot;&gt;We can help customers to design a variety of products, to provide better service.&lt;/span&gt;&lt;/div&gt;
+&lt;/div&gt;
+
+&lt;p&gt;&amp;nbsp;&lt;/p&gt;
+";}s:10:"show_title";s:1:"0";s:9:"layout_id";s:1:"1";s:8:"position";s:8:"showcase";s:6:"status";s:1:"1";s:12:"module_class";s:0:"";s:10:"sort_order";s:1:"1";}}';
+
+
+function aaa($mach){
+	return 's:' . strlen($mach[2]) . ':"' . $mach[2] . '";' ;
+}
+$a =  preg_replace_callback('!s:(\d+):"(.*?)";!s', 'aaa', $a );
+
+$b = unserialize($a);
+var_dump($b);
+?>
